f68cd2c3139688aa6e53e946904e9bb7c6fd173d
Dejan-84 <dekidjurdjev@gmail.com>
2022-05-15 18:54:22 +0200

    Added js for products

---
+++ assets/js/products.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    /*
+    $(document).on('click','.button-quickview',function() {
+
+        var id = $(this).attr('data-id');
+        
+        $.ajax({
+
+            url: 'ajax/product_details.php',
+            method:'POST',
+            dataType:'json',
+            data:{id},
+
+            success: function(response) {
+
+                console.log(response);
+            }
+        })
+    });
+    */
+})
