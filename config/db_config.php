0259878fc985e257a38174e7a748ed25abd58142
Dejan-84 <dekidjurdjev@gmail.com>
2023-11-17 20:33:32 +0100

    Changess

--- config/db_config.php
+++ config/db_config.php
@@ -1,9 +1,12 @@
 <?php
 
 define('DB_SERVER', 'dejandjurdjev.com');
-define('DB_USER', 'u724976138_furniture');
+define('DB_USER', 'u921082012_u921082012_iOZ');
 define('DB_PASSWORD', '#Dejan1984');
-define('DB_NAME', 'u724976138_furniture');
+define('DB_NAME', 'u921082012_furniture');
+
+define('ENVIRONMENT', 'production');
+
 
 
 
