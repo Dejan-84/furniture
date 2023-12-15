a5e3998f357cc7637a861af042a747cd45fb1f05
Dejan-84 <dekidjurdjev@gmail.com>
2023-12-15 19:28:25 +0100

    chs

--- config/db_config.php
+++ config/db_config.php
@@ -9,10 +9,4 @@ define('ENVIRONMENT', 'production');
 
 
 
-/*define('DB_SERVER', 'dejandjurdjev.com');
-define('DB_USER', 'u921082012_u921082012_iOZ');
-define('DB_PASSWORD', '#Dejan1984');
-define('DB_NAME', 'u921082012_furniture');
-
-define('ENVIRONMENT', 'production');*/
 
